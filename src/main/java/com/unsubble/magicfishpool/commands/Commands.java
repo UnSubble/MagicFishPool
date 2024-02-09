281781853acba21565573c24463ce279971bd311
İsmail Kulak <unsubble@gmail.com>
2024-02-09 19:43:31 +0300

    Commands class has created.

---
+++ src/main/java/com/unsubble/magicfishpool/commands/Commands.java
@@ -0,0 +1,25 @@
+package com.unsubble.magicfishpool.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class Commands implements TabExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if (commandSender.isOp()) {
+
+        }
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return null;
+    }
+}
